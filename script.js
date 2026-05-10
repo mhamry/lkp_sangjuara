585166ec3faf4bb7fe544ce43ff489d18d832635
mhamry <hamrymuhammad1977@gmail.com>
2026-05-10 20:29:38 +0700

    ubah color

--- script.js
+++ script.js
@@ -25,9 +25,9 @@ function runTrack(selector, direction, time) {
 
 window.onload = () => {
   runTrack(".track-right", "right", 60);
-  //   runTrack(".track-left", "left", 50);
 };
 
+//batas
 const currentPage = window.location.pathname.split("/").pop();
 
 document.querySelectorAll(".nav-link").forEach((link) => {
@@ -37,3 +37,24 @@ document.querySelectorAll(".nav-link").forEach((link) => {
     link.classList.add("active");
   }
 });
+
+//batas
+
+const navbar = document.querySelector(".navbar");
+const navbarBrand = document.querySelector(".navbar-brand");
+const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+window.onscroll = () => {
+  if (window.scrollY > 20) {
+    navbar.classList.add("sticky");
+    navbarBrand.classList.add("white");
+    navLinks.forEach((link) => {
+      link.classList.add("white");
+    });
+  } else {
+    navbar.classList.remove("sticky");
+    navbarBrand.classList.remove("white");
+    navLinks.forEach((link) => {
+      link.classList.remove("white");
+    });
+  }
+};
