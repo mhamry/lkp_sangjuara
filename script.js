3b3b643fb386fd486b4a1843ce6b0c05687e3ec1
mhamry <hamrymuhammad1977@gmail.com>
2026-05-15 06:59:58 +0700

    m

--- script.js
+++ script.js
@@ -13,16 +13,11 @@
 
 function runTrack(selector, direction, time) {
   const track = document.querySelector(selector);
-
   track.innerHTML += track.innerHTML;
-
   const originalWidth = track.scrollWidth / 2;
-
   track.style.setProperty("--move", `-${originalWidth}px`);
-
   track.style.animation = `run-${direction} ${time}s linear infinite`;
 }
-
 window.onload = () => {
   runTrack(".track-right", "right", 60);
 };
